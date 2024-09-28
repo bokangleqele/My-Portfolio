e5e91240be220141676230e6395d3e65f2b75c71
bokangleqele <bokangleqele7@gmail.com>
2024-09-28 15:58:02 +0100

    First commit

---
+++ app/(front)/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/FrontEnd/Hero";
+import React from "react";
+
+
+export default async function Home() {
+  return (
+    <section className="">
+      <Hero /> 
+     </section>
+  );
+} 
