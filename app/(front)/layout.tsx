2ad60366cee69a7828591265ee656fb9292fef25
bokangleqele <bokangleqele7@gmail.com>
2024-09-28 23:07:03 +0100

    Home page in progress

---
+++ app/(front)/layout.tsx
@@ -0,0 +1,15 @@
+// import Footer from '@/components/FrontEnd/Footer'
+import Navbar from '@/components/FrontEnd/Navbar'
+import React, { ReactNode } from 'react'
+
+
+
+export default async function Layout({children}: {children:ReactNode}) {
+  return (
+    <div>
+      <Navbar />
+       {children}
+       
+    </div>
+  )
+}
