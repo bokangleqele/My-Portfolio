826f71ade6dac1e253c353bb0645d4cd76470c15
bokangleqele <bokangleqele7@gmail.com>
2024-09-29 01:19:13 +0100

    toggle mode mulfunctions

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -1,7 +1,7 @@
 // import Footer from '@/components/FrontEnd/Footer'
 import Navbar from '@/components/FrontEnd/Navbar'
 import React, { ReactNode } from 'react'
-
+import Footer from '@/components/FrontEnd/Footer'
 
 
 export default async function Layout({children}: {children:ReactNode}) {
@@ -9,7 +9,7 @@ export default async function Layout({children}: {children:ReactNode}) {
     <div>
       <Navbar />
        {children}
-       
+       <Footer />
     </div>
   )
 }
