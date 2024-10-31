2bd986f02e671c405726c8d5a8aa3d958e1483e6
bokangleqele <bokangleqele7@gmail.com>
2024-10-31 23:48:31 +0000

    ?

--- components/FrontEnd/about.tsx
+++ components/FrontEnd/about.tsx
@@ -1,22 +1,201 @@
-"use client"; // This directive is required for client components in Next.js
-import React from "react";
+"use client";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export function AboutMe() {
-    return (
-        <div className="p-10">
-            <div className="max-w-screen-xl mx-auto px-4 dark:text-white md:px-8">
-                <div className="max-w-xl space-y-3 text-center mx-auto">
-                    <h1 className="text-3xl font-semibold sm:text-4xl dark:text-white">
-                        About Me
-                    </h1>
-                    <p className="mt-4 text-base">
-                        Hello! I’m Bokang Leqele, an IT technician and software engineering intern passionate about solving complex problems and creating efficient solutions. My journey in tech began with a BSc in Physics and Computer Science, providing a solid foundation in analytical thinking and problem-solving. I've gained hands-on experience in coding, troubleshooting, and systems administration, all skills I continuously hone through my work and side projects.
-                    </p>
-                    <p className="mt-4 text-base">
-                        I aim to bridge the gap between technology and practical applications, striving to make systems work seamlessly. Currently, I’m eager to grow as a developer, learning new technologies and frameworks to better serve in my roles and enhance user experiences. Feel free to connect with me through the details below!
-                    </p>
-                </div>
+  return (
+    <div className="flex flex-col items-center justify-start pt-10">
+      <h1 className="text-3xl font-bold mb-4 text-gray-800">About Me</h1>
+      
+      {/* First Subheading */}
+      <h2 className="text-2xl font-semibold mb-4 text-gray-700">Skills</h2>
+      <div className="flex flex-wrap justify-center gap-4"> {/* Flex container for cards */}
+        {/* First Card */}
+        <div className="max-w-xs w-full group/card">
+          <div
+            className={cn(
+              "cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4",
+              "bg-[url(https://images.unsplash.com/photo-1544077960-604201fe74bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80)] bg-cover"
+            )}
+          >
+            <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-60"></div>
+            <div className="flex flex-row items-center space-x-4 z-10">
+              <Image
+                height="100"
+                width="100"
+                alt="Avatar"
+                src="/manu.png"
+                className="h-10 w-10 rounded-full border-2 object-cover"
+              />
+              <div className="flex flex-col">
+                <p className="font-normal text-base text-gray-50 relative z-10">
+                  Bokang Leqele
+                </p>
+                <p className="text-sm text-gray-400">Front end Developer</p>
+              </div>
             </div>
+            <div className="text content">
+              <h1 className="font-bold text-xl md:text-2xl text-gray-50 relative z-10">
+                Web Development
+              </h1>
+              <p className="font-normal text-sm text-gray-50 relative z-10 my-4">
+               Proficient in HTML, CSS, JavaScript, React, Next.js, Typescript and morden frameworks
+              </p>
+            </div>
+          </div>
         </div>
-    );
+
+        {/* Second Card */}
+        <div className="max-w-xs w-full group/card">
+          <div
+            className={cn(
+              "cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4",
+              "bg-[url(https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80)] bg-cover"
+            )}
+          >
+            <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-60"></div>
+            <div className="flex flex-row items-center space-x-4 z-10">
+              <Image
+                height="100"
+                width="100"
+                alt="Avatar"
+                src="/woman.png" // Replace with the second avatar image
+                className="h-10 w-10 rounded-full border-2 object-cover"
+              />
+              <div className="flex flex-col">
+                <p className="font-normal text-base text-gray-50 relative z-10">
+                  Bokang Leqele
+                </p>
+                <p className="text-sm text-gray-400">Front end Developer</p>
+              </div>
+            </div>
+            <div className=" text content">
+              <h1 className="font-bold text-xl md:text-2xl text-gray-50 relative z-10">
+                Mobile Application
+              </h1>
+              <p className="font-normal text-sm text-gray-50 relative z-10 my-4">
+                Confident with using React Native and Expo go App.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Second Subheading */}
+      <h2 className="text-2xl font-semibold mb-4 text-gray-700 mt-10">Experiences</h2>
+      <div className="flex flex-wrap justify-center gap-4"> {/* Flex container for cards */}
+        {/* Third Card */}
+        <div className="max-w-xs w-full group/card">
+          <div
+            className={cn(
+              "cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4",
+              "bg-[url(https://images.unsplash.com/photo-1544077960-604201fe74bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80)] bg-cover"
+            )}
+          >
+            <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-60"></div>
+            <div className="flex flex-row items-center space-x-4 z-10">
+              <Image
+                height="100"
+                width="100"
+                alt="Avatar"
+                src="/manu.png"
+                className="h-10 w-10 rounded-full border-2 object-cover"
+              />
+              <div className="flex flex-col">
+                <p className="font-normal text-base text-gray-50 relative z-10">
+                  Bokang Leqele
+                </p>
+                <p className="text-sm text-gray-400">Software Engineer</p>
+              </div>
+            </div>
+            <div className="text content">
+              <h1 className="font-bold text-xl md:text-2xl text-gray-50 relative z-10">
+                Software Engineer
+              </h1>
+              <p className="font-normal text-sm text-gray-50 relative z-10 my-4">
+              Currently a software engineer intern at Nucleus Pty Ltd, Maseru, Lesotho
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Fourth Card */}
+        
+      </div>
+
+      {/* Third Subheading */}
+      <h2 className="text-2xl font-semibold mb-4 mt-10 text-gray-700">Educational Background</h2>
+      <div className="flex flex-wrap justify-center gap-4"> {/* Flex container for cards */}
+        {/* First Card */}
+        <div className="max-w-xs w-full group/card">
+          <div
+            className={cn(
+              "cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4",
+              "bg-[url(https://images.unsplash.com/photo-1544077960-604201fe74bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80)] bg-cover"
+            )}
+          >
+            <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-60"></div>
+            <div className="flex flex-row items-center space-x-4 z-10">
+              <Image
+                height="100"
+                width="100"
+                alt="Avatar"
+                src="/manu.png"
+                className="h-10 w-10 rounded-full border-2 object-cover"
+              />
+              <div className="flex flex-col">
+                <p className="font-normal text-base text-gray-50 relative z-10">
+                  Bokang Leqele
+                </p>
+                <p className="text-sm text-gray-400">Bsc Physics & Computer science</p>
+              </div>
+            </div>
+            <div className="text content">
+              <h1 className="font-bold text-xl md:text-2xl text-gray-50 relative z-10">
+                Bsc Physics & Computer Science
+              </h1>
+              <p className="font-normal text-sm text-gray-50 relative z-10 my-4">
+                2024: Obtained a degree in Physics & Computer Science
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Second Card */}
+        <div className="max-w-xs w-full group/card">
+          <div
+            className={cn(
+              "cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4",
+              "bg-[url(https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80)] bg-cover"
+            )}
+          >
+            <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-60"></div>
+            <div className="flex flex-row items-center space-x-4 z-10">
+              <Image
+                height="100"
+                width="100"
+                alt="Avatar"
+                src="/woman.png" // Replace with the second avatar image
+                className="h-10 w-10 rounded-full border-2 object-cover"
+              />
+              <div className="flex flex-col">
+                <p className="font-normal text-base text-gray-50 relative z-10">
+                  Bokang Leqele
+                </p>
+                <p className="text-sm text-gray-400">LGCSE</p>
+              </div>
+            </div>
+            <div className="text content">
+              <h1 className="font-bold text-xl md:text-2xl text-gray-50 relative z-10">
+                LGCSE
+              </h1>
+              <p className="font-normal text-sm text-gray-50 relative z-10 my-4">
+                2019: Successfully completed Lesotho General Certificate of Secondary Education
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,50 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import {
+  default as flattenColorPalette,
+} from "tailwindcss/lib/util/flattenColorPalette";
 
+const addVariablesForColors = ({ addBase, theme }: any) => {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+};
+
+/** @type {import('tailwindcss').Config} */
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
 export default config;
