76f1d47440dfffc454245bd84dd91dd68befa363
bokangleqele <bokangleqele7@gmail.com>
2024-11-12 16:59:38 +0000

    [Hero.tsx] whatsapp and mail links redirect accordingly

--- components/FrontEnd/Hero.tsx
+++ components/FrontEnd/Hero.tsx
@@ -79,10 +79,13 @@ export default function Hero()  {
                             Feel free to reach out and together, let's make the world a better place.
                         </p>
                         <div className="items-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
-                            <a href="javascript:void(0)" className="block py-2 px-4 text-center text-white font-medium bg-green-500 duration-150 hover:bg-green-600 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none">
+                            <a href="https://wa.me/26663325395" className="block py-2 px-4 text-center text-white font-medium bg-green-500 duration-150 hover:bg-green-600 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none"
+                             target="_blank" 
+                                rel="noopener noreferrer"
+                            >
                                 Whatsapp
                             </a>
-                            <a href="javascript:void(0)" className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg md:inline-flex">
+                            <a href="mailto:bokangleqele7@gmail.com" className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg md:inline-flex">
                                 Email
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                     <path fillRule="evenodd" d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z" clipRule="evenodd" />
