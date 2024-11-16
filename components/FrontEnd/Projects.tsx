8b48b85a4eb0c263c6ee49e5ecce2eb36db3d9dc
bokangleqele <bokangleqele7@gmail.com>
2024-11-16 02:08:12 +0000

    [Projects.tsx] edited (line 135 card, index) to just (card) due to deployment failure

--- components/FrontEnd/Projects.tsx
+++ components/FrontEnd/Projects.tsx
@@ -132,7 +132,7 @@ export function ProjectInfo() {
       <h1 className="text-3xl font-bold mb-4 text-gray-800 text-center mx-auto">My Projects</h1>
       
       <ul className="max-w-2xl mx-auto w-full grid grid-cols-1 md:grid-cols-2 items-start gap-4">
-        {cards.map((card, index) => (
+        {cards.map((card) => (
           <motion.div
             layoutId={`card-${card.title}-${id}`}
             key={card.title}
